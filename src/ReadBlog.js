253a571434c0ef63c57828b6605f95d922fba310
Bardala <102490981+Bardala@users.noreply.github.com>
2022-12-22 12:38:43 +0200

    Showing Blogs Content

--- src/ReadBlog.js
+++ src/ReadBlog.js
@@ -1,11 +1,25 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const ReadBlog = () => {
   const { id } = useParams();
+  const {
+    data: blog,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/blogs/" + id);
 
   return (
     <div className="blog-details">
-      <h2>Blog details - {id}</h2>
+      {error && <p>{error}</p>}
+      {isPending && <p>Loading...</p>}
+      {blog && (
+        <article>
+          <h2>{blog.title}</h2>
+          <div>Written by {blog.author}</div>
+          <p>{blog.body}</p>
+        </article>
+      )}
     </div>
   );
 };
