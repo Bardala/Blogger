0c73e3db056a812faaa4c676f07924b57368e42f
Bardala <102490981+Bardala@users.noreply.github.com>
2022-12-22 14:26:47 +0200

    You can create blogs now

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,36 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("Islam");
+
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
+      <form>
+        <label>Blog Title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <label>Blog body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+
+        <label>Blog author:</label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="Islam">Islam</option>
+          <option value="Ali">Ali</option>
+        </select>
+        <button>Add Blog</button>
+      </form>
     </div>
   );
 };
