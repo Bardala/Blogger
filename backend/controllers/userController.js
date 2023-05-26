23c760fe3143e8afa691f3f2f43b866053b7a1d5
Bardala <102490981+Bardala@users.noreply.github.com>
2023-05-26 19:02:18 +0300

    follow user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -72,9 +72,31 @@ const getUserByUsername = async (req, res) => {
   }
 };
 
+//checked
+const followUser = async (req, res) => {
+  try {
+    const user = await User.findOneAndUpdate(
+      { username: req.params.username },
+      { $addToSet: { followers: req.user._id } },
+      { new: true },
+    );
+
+    const currentUser = await User.findOneAndUpdate(
+      { _id: req.user._id },
+      { $addToSet: { following: user._id } },
+      { new: true },
+    );
+
+    res.status(200).json({ user, currentUser });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
 module.exports = {
   signup,
   login,
   getUsers,
   getUserByUsername,
+  followUser,
 };
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,10 +2,12 @@ const express = require("express");
 const {
   getUsers,
   getUserByUsername,
+  followUser,
 } = require("../controllers/userController");
 const router = express.Router();
 
 router.get("/users", getUsers);
 router.get("/users/:username", getUserByUsername);
+router.post("/users/:username/follow", followUser);
 
 module.exports = router;
