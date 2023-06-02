00262f9109bd3ae4943c05d94748287710978bd4
Bardala <102490981+Bardala@users.noreply.github.com>
2023-06-02 06:48:58 +0300

    fix SpaceInfo issue

--- frontend/src/components/SpaceInfo.jsx
+++ frontend/src/components/SpaceInfo.jsx
@@ -3,9 +3,12 @@ const SpaceInfo = ({ members, admins, spaceTitle, state }) => {
     <div className="space-info">
       <h1>{spaceTitle}</h1>
       <p>State: {state}</p>
-      {/* map over the members and admins arrays and join them with commas */}
-      <p>Members: {members.map((member) => member).join(", ")}</p>
-      <p>Admins: {admins.map((admin) => admin).join(", ")}</p>
+      {members?.length > 0 && (
+        <p>Members: {members.map((member) => member).join(", ")}</p>
+      )}
+      {admins?.length > 0 && (
+        <p>Admins: {admins.map((admin) => admin).join(", ")}</p>
+      )}
     </div>
   );
 };
---
+++ frontend/src/pages/CreateSpace.jsx
@@ -0,0 +1,5 @@
+const CreateSpace = () => {
+  return <></>;
+};
+
+export default CreateSpace;
