348a7e3400f0c512bc86244d63b288bfe3930fae
Bardala <102490981+Bardala@users.noreply.github.com>
2023-07-12 17:07:23 +0300

    implement DataStoreDao

--- backend/js-to-ts/dataStore/sql/SqlDataStore.class.ts
+++ backend/js-to-ts/dataStore/sql/SqlDataStore.class.ts
@@ -1,81 +1,111 @@
 import { DataStoreDao } from "..";
 import { User, Blog, Comment, Space, Like } from "../types";
-import mysql from "mysql2/promise";
+import mysql, { RowDataPacket } from "mysql2";
+import { Pool } from "mysql2/promise";
 
 export class SqlDataStore implements DataStoreDao {
-  private connection: mysql.Connection;
+  private pool!: Pool;
 
-  async runDb() {
-    this.connection = await mysql.createConnection({
-      host: process.env.MY_SQL_DB_HOST,
-      user: process.env.MY_SQL_DB_USER,
-      database: process.env.MY_SQL_DB_DATABASE,
-      password: process.env.MY_SQL_DB_PASSWORD,
-    });
+  async runDB() {
+    this.pool = mysql
+      .createPool({
+        host: process.env.MY_SQL_DB_HOST,
+        user: process.env.MY_SQL_DB_USER,
+        database: process.env.MY_SQL_DB_DATABASE,
+        password: process.env.MY_SQL_DB_PASSWORD,
+      })
+      .promise();
 
     return this;
   }
 
-  async createUser(user: User): Promise<User | undefined> {
-    await this.connection.query(
-      `INSERT INTO users (id, username, password, email)
-      VALUES
-      (?,?,?,?)`,
+  async createUser(user: User): Promise<void> {
+    await this.pool.query<RowDataPacket[]>(
+      "INSERT INTO users SET id=?, username=?, password=?, email=?",
       [user.id, user.username, user.password, user.email],
     );
-    return;
   }
 
-  getUserById(userId: string): Promise<User | undefined> {
-    throw new Error("Method not implemented.");
+  async getUserById(userId: string): Promise<User | undefined> {
+    const [rows] = await this.pool.query<RowDataPacket[]>(
+      `SELECT * FROM users WHERE id = ?`,
+      [userId],
+    );
+    return rows[0] as User;
   }
-  getUserByUsername(username: string): Promise<User | undefined> {
-    throw new Error("Method not implemented.");
+
+  async getUserByUsername(username: string): Promise<User | undefined> {
+    const [rows] = await this.pool.query<RowDataPacket[]>(
+      "SELECT * FROM users WHERE username = ?",
+      [username],
+    );
+
+    return rows[0] as User;
   }
-  getUserByEmail(email: string): Promise<User | undefined> {
-    throw new Error("Method not implemented.");
+  async getUserByEmail(email: string): Promise<User | undefined> {
+    const [rows] = await this.pool.query<RowDataPacket[]>(
+      "SELECT * FROM users WHERE email = ?",
+      [email],
+    );
+
+    return rows[0] as User;
   }
 
   async createBlog(blog: Blog): Promise<void> {
-    await this.connection.query(
-      `
-      INSERT INTO blogs (id, title, content, userId, spaceId) 
-      VALUES
-      (?, ?, ?, ?, ?)
-      `,
+    await this.pool.query(
+      "INSERT INTO blogs SET id=?, title=?, content=?, userId=?, spaceId=?",
       [blog.id, blog.title, blog.content, blog.userId, blog.spaceId],
     );
   }
 
-  getSpaceBlogs(spaceId: string): Promise<Blog[]> {
-    throw new Error("Method not implemented.");
+  async getSpaceBlogs(spaceId: string): Promise<Blog[]> {
+    const [rows] = await this.pool.query<RowDataPacket[]>(
+      "SELECT * FROM blogs WHERE spaceId = ?",
+      [spaceId],
+    );
+    return rows as Blog[];
   }
-  getBlog(blogId: string): Promise<Blog | undefined> {
-    throw new Error("Method not implemented.");
+
+  async getBlog(blogId: string): Promise<Blog | undefined> {
+    const [rows] = await this.pool.query<RowDataPacket[]>(
+      "SELECT * FROM blogs WHERE id=?",
+      [blogId],
+    );
+    return rows[0] as Blog;
   }
-  deleteBlog(blogId: string): Promise<void> {
-    throw new Error("Method not implemented.");
+
+  async deleteBlog(blogId: string): Promise<void> {
+    await this.pool.query<RowDataPacket[]>("DELETE FROM blogs WHERE id=?", [
+      blogId,
+    ]);
   }
-  likeBlog(blogId: string): Promise<void> {
-    throw new Error("Method not implemented.");
+
+  async getUserBlogs(userId: string): Promise<Blog[]> {
+    const [rows] = await this.pool.query<RowDataPacket[]>(
+      "SELECT * FROM blogs WHERE userId = ?",
+      [userId],
+    );
+    return rows[0] as Blog[];
   }
-  getUserBlogs(userId: string): Promise<Blog[]> {
-    throw new Error("Method not implemented.");
+
+  async createComment(comment: Comment): Promise<void> {
+    await this.pool.query<RowDataPacket[]>(
+      "INSERT INTO comments SET id=?, blogId=?, userId=?, content=?",
+      [comment.id, comment.blogId, comment.authorId, comment.content],
+    );
   }
-  createComment(comment: Comment): Promise<void> {
-    throw new Error("Method not implemented.");
-  }
-  getComments(blogId: string): Promise<Comment[]> {
-    throw new Error("Method not implemented.");
+
+  async getComments(blogId: string): Promise<Comment[]> {
+    const [rows] = await this.pool.query<RowDataPacket[]>(
+      "SELECT * FROM comments WHERE blogId = ?",
+      [blogId],
+    );
+    return rows[0] as Comment[];
   }
 
   async createSpace(space: Space): Promise<void> {
-    await this.connection.query(
-      `
-      INSERT INTO spaces (description, id, name, ownerId, status)
-      VALUES
-      (?,?,?,?,?)
-    `,
+    await this.pool.query(
+      "INSERT INTO spaces SET description=?, id=?, name=?, ownerId=?, status=?",
       [space.description, space.id, space.name, space.ownerId, space.status],
     );
   }
@@ -83,141 +113,48 @@ export class SqlDataStore implements DataStoreDao {
   getDefaultSpace(): Promise<Space | undefined> {
     throw new Error("Method not implemented.");
   }
-  getSpace(spaceId: string): Promise<Space | undefined> {
-    throw new Error("Method not implemented.");
+
+  async getSpace(spaceId: string): Promise<Space | undefined> {
+    const [rows] = await this.pool.query<RowDataPacket[]>(
+      "SELECT * FROM spaces WHERE id = ?",
+      [spaceId],
+    );
+    return rows[0] as Space;
   }
-  updateSpace(spaceId: string): Promise<Space | undefined> {
-    throw new Error("Method not implemented.");
+
+  async updateSpace(space: Space): Promise<Space | undefined> {
+    const [rows] = await this.pool.query<RowDataPacket[]>(
+      "UPDATE spaces SET name=?, status=?, description=? WHERE id=?",
+      [space.name, space.status, space.description, space.id],
+    );
+    return rows[0] as Space;
   }
-  joinSpace(memberId: string): Promise<void> {
-    throw new Error("Method not implemented.");
+
+  async joinSpace(spaceId: string, memberId: string): Promise<void> {
+    await this.pool.query<RowDataPacket[]>(
+      "UPDATE spaces SET members = members + ? WHERE id = ?",
+      [memberId, spaceId],
+    );
   }
-  deleteSpace(spaceId: string): Promise<void> {
-    throw new Error("Method not implemented.");
+
+  async deleteSpace(spaceId: string): Promise<void> {
+    await this.pool.query<RowDataPacket[]>("DELETE FROM spaces WHERE id=?", [
+      spaceId,
+    ]);
   }
-  getSpaces(userId: string): Promise<Space[]> {
-    throw new Error("Method not implemented.");
+
+  async getSpaces(userId: string): Promise<Space[]> {
+    const [rows] = await this.pool.query<RowDataPacket[]>(
+      "SELECT * FROM spaces WHERE userId = ?",
+      userId,
+    );
+    return rows[0] as Space[];
   }
-  createLike(like: Like): Promise<void> {
-    throw new Error("Method not implemented.");
+
+  async createLike(like: Like): Promise<void> {
+    await this.pool.query<RowDataPacket[]>(
+      "INSERT INTO likes id=?, blogId=?, userId=?",
+      [like.id, like.blogId, like.userId],
+    );
   }
 }
-
-// private async runMigration() {
-//   const migrationDir = path.join(__dirname, "migrations");
-
-//   fs.readdirSync(migrationDir)
-//     .sort()
-//     .forEach((file) => {
-//       const migrationFilePath = path.join(migrationDir, file);
-//       const migrationSql = fs.readFileSync(migrationFilePath, "utf8");
-
-//       this.connection.query(migrationSql, (err) => {
-//         if (err) throw err;
-//         console.log(`Migration applied: ${file}`);
-//       });
-//     });
-// }
-// private async runMigration() {
-//   const migrationDir = path.join(__dirname, "migrations");
-
-//   try {
-//     // Start a transaction
-//     await new Promise<void>((resolve, reject) => {
-//       this.connection.beginTransaction((err) => {
-//         if (err) reject(err);
-//         else resolve();
-//       });
-//     });
-
-//     // Create a table to store the migration history
-//     await new Promise<void>((resolve, reject) => {
-//       this.connection.query(
-//         "CREATE TABLE IF NOT EXISTS migrations (name VARCHAR(255) PRIMARY KEY, date DATETIME)",
-//         (err) => {
-//           if (err) reject(err);
-//           else resolve();
-//         },
-//       );
-//     });
-
-//     // Read and execute the migration scripts
-//     fs.readdirSync(migrationDir)
-//       .sort()
-//       .forEach(async (file) => {
-//         const migrationFilePath = path.join(migrationDir, file);
-//         const migrationSql = fs.readFileSync(migrationFilePath, "utf8");
-
-//         // Check if the migration script has already been applied
-//         const result = await new Promise((resolve, reject) => {
-//           this.connection.query(
-//             "SELECT * FROM migrations WHERE name = ?",
-//             [file],
-//             (err, rows) => {
-//               if (err) reject(err);
-//               else resolve(rows);
-//             },
-//           );
-//         });
-
-//         // If not, execute the script and insert a row into the migrations table
-//         if (result.length === 0) {
-//           await new Promise<void>((resolve, reject) => {
-//             this.connection.query(migrationSql, (err) => {
-//               if (err) reject(err);
-//               else resolve();
-//             });
-//           });
-
-//           await new Promise<void>((resolve, reject) => {
-//             this.connection.query(
-//               "INSERT INTO migrations (name, date) VALUES (?, NOW())",
-//               [file],
-//               (err) => {
-//                 if (err) reject(err);
-//                 else resolve();
-//               },
-//             );
-//           });
-
-//           console.log(`Migration applied: ${file}`);
-//         } else {
-//           console.log(`Migration skipped: ${file}`);
-//         }
-//       });
-
-//     // Commit the transaction
-//     await new Promise<void>((resolve, reject) => {
-//       this.connection.commit((err) => {
-//         if (err) reject(err);
-//         else resolve();
-//       });
-//     });
-//   } catch (error) {
-//     // Catch any errors and log them
-//     console.error(error);
-
-//     // Rollback the transaction
-//     await new Promise<void>((resolve, reject) => {
-//       this.connection.rollback((err) => {
-//         if (err) reject(err);
-//         else resolve();
-//       });
-//     });
-//   } finally {
-//     // Close the connection
-//     await new Promise<void>((resolve, reject) => {
-//       this.connection.end((err) => {
-//         if (err) reject(err);
-//         else resolve();
-//       });
-//     });
-//   }
-// }
-
-// async runDb() {
-//   // Call the runMigration() method to perform the initial migration
-//   await this.runMigration();
-//   return this;
-//   // Your other code logic here
-// }
